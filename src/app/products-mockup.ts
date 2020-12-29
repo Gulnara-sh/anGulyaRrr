3313a29760a8661ddd31e05687be5942933fd0c6
Gulnara <g.shayakhmetova547@gmail.com>
2020-12-29 18:50:45 +0400

    final

---
+++ src/app/products-mockup.ts
@@ -0,0 +1,46 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'Душевное равновесие',
+    img: './assets/img/products/item_1.jpg',
+    price: 10,
+    description: 'Состав:\nИван-чай «Для душевного равновесия» с мелиссой, душицей и курильским чаем. 50 г.\nДжем-десерт бананово-клубничный. 200 г.\nЯгоды годжи. 10 г.\nСемена чиа. 10 г.'
+  },
+  {
+    id: 2,
+    name: 'Жемчужина в снегах',
+    img: './assets/img/products/item_2.jpg',
+    price: 11,
+    description: 'Состав:\nКрем-баттер для рук «Глубокое питание и восстановление». 50мл.\nМаска для рук «Жемчужная» глубокое увлажнение + постактиватор. 15мл/5мл.\nГель для волос «Лёгкое расчёсывание». 100мл.'
+  },
+  {
+    id: 3,
+    name: 'Кусочек лета в зимний день',
+    img: './assets/img/products/item_3.jpg',
+    price: 15,
+    description: 'Состав:\nСогревающий чай с имбирём, корицей, плодами кэроба и цветками лаванды. 75г.\nДжем из бананов и клубники. 200г.\nШоколад из обжаренного кэроба. 75г.\nБатончик с изюмом, клюквой, яблоком, кешью, миндалём, инжиром и семенами подсолнечника. 35г.'
+  },
+  {
+    id: 4,
+    name: 'Название продукта',
+    img: './assets/img/products/item_1.jpg',
+    price: 17,
+    description: 'description text'
+  },
+  {
+    id: 5,
+    name: 'Название продукта',
+    img: './assets/img/products/item_2.jpg',
+    price: 6,
+    description: 'description text'
+  },
+  {
+    id: 6,
+    name: 'Название продукта',
+    img: './assets/img/products/item_3.jpg',
+    price: 14,
+    description: 'description text'
+  },
+];
